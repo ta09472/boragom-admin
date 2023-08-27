bc49f865e753a6cea3e3a33b7098ec8150e7f23c
beomsuKim <ta09472@naver.com>
2023-08-28 00:44:17 +0900

    주석처리

--- src/app/management/page.tsx
+++ src/app/management/page.tsx
@@ -2,9 +2,10 @@ import Reservation from "@/components/Reservation/Reservation";
 import { TReservation } from "@/types/model/order/type";
 
 export default async function Management() {
-  const data: TReservation[] = await (
-    await fetch("http://localhost:3000/api/order")
-  ).json();
+  // const data: TReservation[] = await (
+  //   await fetch("http://localhost:3000/api/order")
+  // ).json();
 
-  return data?.map((v) => <Reservation reservationInfo={v} key={v.id} />);
+  // return data?.map((v) => <Reservation reservationInfo={v} key={v.id} />);
+  return <div>management</div>;
 }
